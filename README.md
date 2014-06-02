32f63f31dc769f7defc9163c950a3adb55fcf900
craigpestell <craigpestell@gmail.com>
2014-06-01 18:35:28 -0700

    More code clean up.

--- README.md
+++ README.md
@@ -6,29 +6,19 @@ At the top of the file there should be a short introduction and/ or overview tha
 
 ## Code Example
 
-//Call the initialize function (in bicycle-parking.js) on the Google Map's load event.
+Your HTML page must have the #map and #slide elements defined (see supplied index.html).
+Call the initialize function (in bicycle-parking.js) on the Google Map's load event:
 google.maps.event.addDomListener(window, 'load', SfBikeParking.initialize);
-
-## Motivation
-
-A short description of the motivation behind the creation and maintenance of the project. This should explain **why** the project exists.
 
 ## Installation
 
-Provide code examples and explanations of how to get the project.
-
-## API Reference
-
-Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+The files for this project can be pulled from this git repository: https://github.com/craigpestell/bicycle-parking.git
+Upload all files to the HTTP server directory as your choice.  Ensure all files are readable by your HTTP server's user.
 
 ## Tests
 
 Describe and show how to run the tests with code examples.
 
-## Contributors
-
-Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
-
 ## License
 
-A short snippet describing the license (MIT, Apache, etc.)
+GNU General Public License (GPL)
--- bicycle-parking.js
+++ bicycle-parking.js
@@ -4,51 +4,53 @@ var SfBikeParking = SfBikeParking || {};
  * Create a new map object, decorate it, and populate data.
  */
 SfBikeParking.initialize = function (){
-       var mapOptions = {
-		center: new google.maps.LatLng(37.7833, -122.4167),
-        	zoom: 16,
-		scaleControl: true
-        };
-	map = new google.maps.Map(document.getElementById("map-canvas"),
-		mapOptions);
+  var mapOptions = {
+    center: new google.maps.LatLng(37.7833, -122.4167),
+    zoom: 16,
+    scaleControl: true
+  };
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-	map['marker'] = new google.maps.Marker({
-		map: map
-	});
-	map.marker.setVisible(false);
-	map.marker.bindTo('position', map, 'center'); 
+  map['marker'] = new google.maps.Marker({
+    map: map
+  });
+  map.marker.setVisible(false);
+  map.marker.bindTo('position', map, 'center'); 
 
-	map['circle'] = new google.maps.Circle({
-		strokeColor: "#0000FF",
-		strokeOpacity: 0.8,
-		strokeWeight: 2,
-		fillColor: "#0000FF",
-		fillOpacity: 0.25,
-		map: map,
-		center: map.getCenter(),
-		radius: 500
-	});
-	map.circle.bindTo('center', map.marker, 'position');
+  map['circle'] = new google.maps.Circle({
+    strokeColor: "#0000FF",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#0000FF",
+    fillOpacity: 0.25,
+    map: map,
+    center: map.getCenter(),
+    radius: 500
+  });
+  map.circle.bindTo('center', map.marker, 'position');
 
-	$("#slide").slider({
-		orientation: "horizontal",
-		range: "min",
-		max: 3000,
-		min: 500,
-		value: 500,
-		slide: function (event, ui) {
-			SfBikeParking.updateRadius(map.circle, ui.value);
-		}
-	});
-	$.get( "http://data.sfgov.org/resource/w969-5mn4.json?status=COMPLETE&status_detail=INSTALLED", function( data ) {
-		if (!data.length) {
-			alert('There was a problem downloading the San Francisco Bicycle Parking dataset.'); 
-		} else {
-			map['bicycleCoords'] = data;
-			console.log(data);
-			google.maps.event.addListener(map, 'idle', SfBikeParking.showMarkers);
-		}
-	});
+  $("#slide").slider({
+    orientation: "horizontal",
+    range: "min",
+    max: 3000,
+    min: 500,
+    value: 500,
+    slide: function (event, ui) {
+      SfBikeParking.updateRadius(map.circle, ui.value);
+    }
+  });
+  var url = 'http://data.sfgov.org/resource/w969-5mn4.json?' +
+            'status=COMPLETE&status_detail=INSTALLED';
+  $.get(url, function( data ) {
+    if (!data.length) {
+      alert('There was a problem downloading the San Francisco Bicycle ' +
+            'Parking dataset.'); 
+    } else {
+      map['bicycleCoords'] = data;
+      console.log(data);
+      google.maps.event.addListener(map, 'idle', SfBikeParking.showMarkers);
+    }
+  });
 };
 
 /**
@@ -57,9 +59,9 @@ SfBikeParking.initialize = function (){
  * @param rad radius to set.
  */
 SfBikeParking.updateRadius = function (circle, rad) {
-	circle.setRadius(rad);
-	google.maps.event.trigger(map, 'idle');
-}; 
+  circle.setRadius(rad);
+  google.maps.event.trigger(map, 'idle');
+};
 
 /**
  * Convert degrees to radians.
@@ -67,7 +69,7 @@ SfBikeParking.updateRadius = function (circle, rad) {
  * @return float radians
  */
 SfBikeParking.rad = function(x) {
-	return x * Math.PI / 180;
+  return x * Math.PI / 180;
 };
 
 /**
@@ -75,57 +77,58 @@ SfBikeParking.rad = function(x) {
  * @param event event which triggered this function.
  */
 SfBikeParking.showMarkers = function (event) {
-	var lat = map.getCenter().k;
-	var lng = map.getCenter().A;
-	var R = 6371; // radius of earth in km
-	var distances = [];
-	var closest = new Array();
-	closest[0] = -1;
-	coords = map.bicycleCoords;
-	//Set bicycle racks distance to center
-	for (i=0; i < coords.length; i++) {
-		var mlat = coords[i].coordinates.latitude;
-		var mlng = coords[i].coordinates.longitude;
-		var dLat  = SfBikeParking.rad(mlat - lat);
-		var dLong = SfBikeParking.rad(mlng - lng);
-		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-			Math.cos(SfBikeParking.rad(lat)) *
-			Math.cos(SfBikeParking.rad(lat)) *
-			Math.sin(dLong/2) * Math.sin(dLong/2);
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		var d = R * c;
-		distances[i] = d;
-		coords[i]['distance'] = d;
-	}
-	coords.sort(function (a, b) {
-		return a.distance - b.distance;
-	});
+  var lat = map.getCenter().k;
+  var lng = map.getCenter().A;
+  var R = 6371; // radius of earth in km
+  var distances = [];
+  coords = map.bicycleCoords;
+  //Set bicycle parking area's distance to center of map/circle.
+  for (i=0; i < coords.length; i++) {
+    var mlat = coords[i].coordinates.latitude;
+    var mlng = coords[i].coordinates.longitude;
+    var dLat  = SfBikeParking.rad(mlat - lat);
+    var dLong = SfBikeParking.rad(mlng - lng);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(SfBikeParking.rad(lat)) *
+      Math.cos(SfBikeParking.rad(lat)) *
+      Math.sin(dLong/2) * Math.sin(dLong/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    distances[i] = d;
+    coords[i]['distance'] = d;
+  }
+  coords.sort(function (a, b) {
+    return a.distance - b.distance;
+  });
 
-	//clear markers
-	if (map.hasOwnProperty("markers")) {
-		for (i=0; i<map.markers.length; i++) {
-			map.markers[i].setMap(null);
-		}
-		map.markers = [];
-	}
+  //clear markers
+  if (map.hasOwnProperty("markers")) {
+    for (i=0; i<map.markers.length; i++) {
+      map.markers[i].setMap(null);
+    }
+    map.markers = [];
+  }
 
-	for (i=0; i < coords.length; i++) {
-		c = coords[i];
-		var cLatlng = new google.maps.LatLng(c.coordinates.latitude, c.coordinates.longitude);
-		var distanceBtwn = google.maps.geometry.spherical.computeDistanceBetween(cLatlng, map.getCenter());
-		if (distanceBtwn > map.circle.radius) {
-			break;
-		} else {
-			var marker = new google.maps.Marker({
-				position: cLatlng,
-				map: map,
-				title: c.location_name + ', ' + c.yr_inst + ', type: ' + c.bike_parking + ', spaces: ' + c.spaces
-			});
-			if (!map.hasOwnProperty("markers")) {
-				map['markers'] = new Array(marker);
-			} else {
-				map.markers.push(marker);
-			}
-		}
-	}
+  for (i=0; i < coords.length; i++) {
+    c = coords[i];
+    var cLatlng = new google.maps.LatLng(c.coordinates.latitude,
+                                         c.coordinates.longitude);
+    var distanceBtwn = google.maps.geometry.spherical.computeDistanceBetween(
+                           cLatlng, map.getCenter());
+    if (distanceBtwn > map.circle.radius) {
+      break;
+    } else {
+      var marker = new google.maps.Marker({
+        position: cLatlng,
+        map: map,
+        title: c.location_name + ', ' + c.yr_inst +
+              ', type: ' + c.bike_parking + ', spaces: ' + c.spaces
+      });
+      if (!map.hasOwnProperty("markers")) {
+        map['markers'] = new Array(marker);
+      } else {
+        map.markers.push(marker);
+      }
+    }
+  }
 };
